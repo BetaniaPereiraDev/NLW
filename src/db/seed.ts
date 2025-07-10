6ae7eded1586539753414d1502b08541f2f2c047
Luis Augusto <luisaugustoamancio@gmail.com>
2025-07-10 14:25:48 -0300

    feat: add rooms listen

---
+++ src/db/seed.ts
@@ -0,0 +1,24 @@
+import { reset, seed } from 'drizzle-seed'
+import { db } from './connection.ts'
+import { schema } from './schema/index.ts'
+import postgres from 'postgres'
+const sql = postgres()
+
+await reset(db, schema)
+
+await seed(db, schema).refine(f =>{
+    return{
+        rooms: {
+            count: 20,
+            columns: {
+                name: f.companyName(),
+                description: f.loremIpsum(),
+            },
+        },
+    }
+})
+
+await sql.end()
+
+// biome-ignore lint/suspicious/noConsole: only used in dev
+console.log('Database seeded')
